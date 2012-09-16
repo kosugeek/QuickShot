0dce568ecbbea89460e62e04c8503fa1529f9871
mikamikuh <mkouhei0910@gmail.com>
2012-09-16 15:12:06 +0900

    シーンが起動した後、ランダムな秒数を待って音が出るように変更

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -12,18 +12,37 @@ enum State
 
 public class GameManager : MonoBehaviour
 {
+    static System.Random rnd = new System.Random();
+
+    // 最低timeMin秒以上、ランダムでtimeRangeの範囲で音を出す
+    public int timeMin = 3;
+    public int timeRange = 5;
+
+    // 計測中の時間
+    public float currentTime;
+    public AudioClip startClip;
+    public AudioClip playingClip;
+    public GameObject icon;
+
     // ゲーム開始状態
     private State gameState = State.READY;
-    public GameObject startLogo;
 
-    // Use this for initialization
+    //
     void Start ()
     {
-
+        // ゲーム終了まで
+        currentTime = Time.time + timeMin + (float)rnd.Next(timeRange);
+        AudioSource.PlayClipAtPoint(startClip, transform.position);
     }
 
     // Update is called once per frame
     void Update ()
     {
+        if (Time.time > currentTime && State.READY == gameState)
+        {
+            AudioSource.PlayClipAtPoint(playingClip, transform.position);
+            icon.renderer.enabled = true;
+            gameState = State.PLAYING;
+        }
     }
 }
